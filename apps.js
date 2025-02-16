ac1d5e105f9e7deb9eb2028e05c17246cd27fc53
Muhammad Arsalan Rao <marsalanrao007@gmail.com>
2025-02-16 15:21:18 +0500

    BLUB ON & OFF

---
+++ apps.js
@@ -0,0 +1,14 @@
+document.getElementById("bic")
+
+
+let onOff = document.getElementById("bic")
+function blubOn(){
+    let on = document.getElementById("bic")
+    on.setAttribute("src", "./on.jpg")
+}
+
+
+function blubOff (){
+    let Off=document.getElementById("bic")
+    Off.setAttribute("Src", "./off.jpg")
+}
